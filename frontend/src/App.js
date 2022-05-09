c63e0c68b963c8c9b211ef459aaf7712a4ea12de
udayarajurs <udayarajurs7@gmail.com>
2022-05-09 23:34:44 +0530

    Router done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import {useParams} from 'react-router-dom';
+
+function ProductScreen() {
+  const params = useParams();
+  const {slug} = params;
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
+}
+
+export default ProductScreen;
