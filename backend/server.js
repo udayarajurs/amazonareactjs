9df90e09760f992fe572c611596898cce10aadc1
udayarajurs <udayarajurs7@gmail.com>
2022-05-15 00:16:19 +0530

    19 video done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((err) => {
+    console.log(err.message)
+  });
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);   
