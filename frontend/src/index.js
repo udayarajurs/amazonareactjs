a8db4dc94331f5102a55f6fca19457137a10beab
udayarajurs <udayarajurs7@gmail.com>
2022-05-15 18:56:30 +0530

    video 30 done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {HelmetProvider} from 'react-helmet-async';
+import {PayPalScriptProvider} from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <StoreProvider>
       <HelmetProvider>
-        <App />
+        <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
       </HelmetProvider>
     </StoreProvider>
   </React.StrictMode>,
