f50bb524505eaf5c1d16baeab93637f44fa94096
udayarajurs <udayarajurs7@gmail.com>
2022-05-15 17:35:48 +0530

    Video 28 done

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import seedRouter from './routes/seedRouter.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -27,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
