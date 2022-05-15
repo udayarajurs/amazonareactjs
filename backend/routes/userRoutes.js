9474263431b783de7ef7afe7caab23862533ea20
udayarajurs <udayarajurs7@gmail.com>
2022-05-15 19:37:47 +0530

    32 video done

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
-import { generateToken } from '../utils.js';
+import { generateToken , isAuth} from '../utils.js';
 import expressAsyncHandler from 'express-async-handler';
 
 const userRouter = express.Router();
@@ -45,5 +45,31 @@ userRouter.post(
                 });
     })
 )
+
+userRouter.put(
+  '/profile',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  })
+);
 
 export default userRouter;
