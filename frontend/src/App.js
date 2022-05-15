8e6ec2f452fb68480cb63e58920c3d427581c738
udayarajurs <udayarajurs7@gmail.com>
2022-05-15 16:12:07 +0530

    Video 25 done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import SigninScreen from './screens/SigninScreen';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SignupScreen from './screens/SignupScreen';
 function App() {
   const {state, dispatch: ctxDispatch} = useContext(Store);
   const {cart, userInfo} = state;
@@ -75,6 +76,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
               <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
