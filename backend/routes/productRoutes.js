f0adadd2013986c1c5e697462a4b4e55fe47aadf
udaya raj urs G <Udayarajurs7@gmail.com>
2022-05-16 16:43:33 +0530

    video 34 done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import expressAsyncHandler from 'express-async-handler';
 
 const productRouter = express.Router();
 
@@ -7,6 +8,14 @@ productRouter.get('/', async (req, res) => {
   const products = await Product.find();
   res.send(products);
 });
+
+productRouter.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
 
 productRouter.get('/slug/:slug', async (req, res) => {
   const product = await Product.findOne({ slug: req.params.slug });
