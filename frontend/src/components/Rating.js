f56bc9f6b378c4d760c01de9ea481b8bd5ac05a8
udaya raj urs G <Udayarajurs7@gmail.com>
2022-05-16 17:12:19 +0530

    video 35 done

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,5 @@
 function Rating(props) {
-  const {rating, numReviews} = props;
+  const {rating, numReviews, caption} = props;
   return (
     <div className="rating">
       <span>
@@ -57,9 +57,12 @@ function Rating(props) {
           }
         />
       </span>
-      <span> {numReviews} reviews</span>
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{' ' + numReviews + ' reviews'}</span>
+      )}
     </div>
   );
 }
-
 export default Rating;
