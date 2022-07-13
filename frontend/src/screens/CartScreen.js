8d55f6bdf912876549e1f8f53b05afb0a63b0a94
Udaya raj urs <udayarajurs7@gmail.com>
2022-07-14 00:53:13 +0530

    Revert "Fixes"

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -10,6 +10,7 @@ import Card from 'react-bootstrap/Card';
 import {Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
 
+
 export default function CartScreen() {
   const navigate = useNavigate();
   const {state, dispatch: ctxDispatch} = useContext(Store);
@@ -45,7 +46,7 @@ export default function CartScreen() {
       <Helmet>
         <title>Shopping Cart</title>
       </Helmet>
-      <h2>Shopping Cart</h2>
+      <h1>Shopping Cart</h1>
       <Row>
         <Col md={8}>
           {cartItems.length === 0 ? (
